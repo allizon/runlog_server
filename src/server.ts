8806ab92e9febaa81365774e62fabfd389263258
Allison Holt <alholt@akamai.com>
2020-08-28 05:55:01 -0400

    [v2-sequelize] Testing Sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,19 @@
 import express, { response } from "express";
-import { Client } from "pg";
+import { Sequelize } from "sequelize";
 
 const app = express();
 const port = 3000;
 
-const client = new Client({
-  user: "allizon",
-  password: "AvengersAssemble!!",
-  host: "db",
-  port: 5432,
-  database: "ts_server_test",
-});
+const sequelize = new Sequelize(
+  "postgres://allizon:AvengersAssemble!!@db:5432/ts_server_test"
+);
 
-const pgTest = async (response) => {
+const sequelizeTest = async (response) => {
   try {
-    await client.connect();
-    const res = await client.query("SELECT * FROM users");
-    response.send(res.rows);
-    await client.end();
+    await sequelize.authenticate();
+    response.send("Connection has been established successfully");
   } catch (error) {
-    response.send("Error: " + error);
+    response.send(error.toString());
   }
 };
 
@@ -30,10 +24,7 @@ app.get("/", (request, response) => {
 });
 
 app.get("/pg", (request, response) => {
-  // response.send("Here we beeee - pg test - and here?!;");
-  // response.send(client);
-  // response.send("did that work?");
-  pgTest(response);
+  sequelizeTest(response);
 });
 
 app.listen(port, () => {
